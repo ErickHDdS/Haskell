96e7d8af517b3edee1c9ea69df1570f440bf5884
ErickHDdS <erickhenriquedds@gmail.com>
2021-08-18 15:22:36 -0300

    FIX: Ajustando alguns programas

--- disjuntas.hs
+++ disjuntas.hs
@@ -1,12 +1,12 @@
+verificaDisjuntas :: Int -> [Int] -> Bool
+verificaDisjuntas c [] = False
+verificaDisjuntas c (d:f)
+ | c == d = True
+ | otherwise = verificaDisjuntas c f
+
 disjuntas :: [Int] -> [Int] -> Bool
 disjuntas a [] = True
 disjuntas [] b = True
 disjuntas (a:b) x
  | verificaDisjuntas a x = False
  | otherwise = disjuntas b x
-
-verificaDisjuntas :: Int -> [Int] -> Bool
-verificaDisjuntas c [] = False
-verificaDisjuntas c (d:f)
- | c == d = True
- | otherwise = verificaDisjuntas c f
--- removerFim.hs
+++ removerFim.hs
@@ -1,9 +1,9 @@
-removerFim :: Int -> [Int] -> [Int]
-removerFim a [] = []
-removerFim 0 b = b
-removerFim a b = removerFim (a - 1) (retiraUmElemento b)
-
 retiraUmElemento :: [Int] -> [Int]
 retiraUmElemento [] = []
 retiraUmElemento [x] = []
 retiraUmElemento (x:y) = x:retiraUmElemento y
+
+removerFim :: Int -> [Int] -> [Int]
+removerFim a [] = []
+removerFim 0 b = b
+removerFim a b = removerFim (a - 1) (retiraUmElemento b)
